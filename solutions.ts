3096348b14fdd2006f96f7b3e50d541aacd900b7
HoridasPro <horidassarker6@gmail.com>
2026-05-07 13:58:11 +0600

    added to problem no 4 and problem no 5

--- solutions.ts
+++ solutions.ts
@@ -26,3 +26,36 @@ const checkType = (input: StringOrNumber) => {
 };
 const checkTypes = checkType("Hello");
 console.log(checkTypes);
+
+// Problem Solving 4
+interface IuserInfo {
+  id: number;
+  name: string;
+  age: number;
+}
+const getProperty = <Tkey extends keyof IuserInfo>(
+  obj: IuserInfo,
+  key: Tkey,
+) => {
+  return obj[key];
+};
+const user = { id: 1, name: "John Doe", age: 21 };
+const getName = getProperty(user, "name");
+console.log(getName);
+
+// Problem Solving 5
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+const toggleReadStatus = (Book: Book) => {
+  return { ...Book, isRead: true };
+};
+const myBook = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+};
+const getBookInfo = toggleReadStatus(myBook);
+console.log(getBookInfo);
