90127a077d2d6cecbda9c386e8fea568bc00811b
HoridasPro <horidassarker6@gmail.com>
2026-05-07 20:42:43 +0600

    added to final updated

--- solutions.ts
+++ solutions.ts
@@ -3,16 +3,14 @@ const filterEvenNumbers = (arr: number[]) => {
   const evenNumbers = arr.filter((num) => num % 2 === 0);
   return evenNumbers;
 };
-const filteringNumbers = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
-console.log(filteringNumbers);
+filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 
 // Problem Solving 2
 const reverseString = (str: string) => {
   const reverse = str.split("").reverse().join("");
   return reverse;
 };
-const reverseStr = reverseString("typescript");
-console.log(reverseStr);
+reverseString("typescript");
 
 // Problem Solving 3
 type StringOrNumber = string | number;
@@ -23,8 +21,7 @@ const checkType = (input: StringOrNumber): string => {
     return "Number";
   }
 };
-const checkTypes = checkType("Hello");
-console.log(checkTypes);
+checkType("Hello");
 
 // Problem Solving 4
 interface IuserInfo {
@@ -39,8 +36,7 @@ const getProperty = <Tkey extends keyof IuserInfo>(
   return obj[key];
 };
 const user = { id: 1, name: "John Doe", age: 21 };
-const getName = getProperty(user, "name");
-console.log(getName);
+getProperty(user, "name");
 
 // Problem Solving 5
 interface Book {
@@ -56,8 +52,7 @@ const myBook = {
   author: "Jane Doe",
   publishedYear: 2024,
 };
-const getBookInfo = toggleReadStatus(myBook);
-console.log(getBookInfo);
+toggleReadStatus(myBook);
 
 // Prbolem Solving 6
 class Person {
@@ -79,12 +74,10 @@ class Student extends Person {
   }
 }
 const student = new Student("Alice", 20, "A");
-const stuInfo = student.getDetails();
-console.log(stuInfo);
+student.getDetails();
 
 // Problem Solving 7
 const getIntersection = (arr1: number[], arr2: number[]) => {
   return arr1.filter((num) => arr2.includes(num));
 };
-const commonArr = getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
-console.log(commonArr);
+getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
